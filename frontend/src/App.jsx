08f0dba301e0d7e48943d4ecb9f8ad92d43e4298
Mujahid087 <mujahidmahedi10@gmail.com>
2025-03-21 10:24:11 +0530

    creating the files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import ProfilePage from "./pages/ProfilePage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from './pages/SettingsPage'
+import { axiosInstance } from './lib/axios'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+
+const App = () => {
+  const {authUser,checkAuth,isCheckingAuth}=useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log(authUser)
+  if(isCheckingAuth && !authUser){
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin"/>
+
+    </div>
+
+  }
+
+
+  return (
+   <>
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
+    <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" />} />
+    <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
+    <Route path="/settings" element={<SettingsPage/>}/>
+    <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login" />} />
+   </Routes>
+
+   <Toaster/>
+   
+   
+   
+   </>
+  )
+}
+
+export default App
